c7e7a2c53d4bb4de1821b1a6bb55c7cb49d7cd48
leeyulgok <dbfrhr20@gmail.com>
2023-02-27 08:19:07 +0900

    Nodejs Practice Version2

--- models/cart.js
+++ models/cart.js
@@ -1,68 +1,14 @@
-const fs = require("fs");
-const path = require("path");
+const Sequelize = require('sequelize');
 
-const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+const sequelize = require('../util/database');
 
-module.exports = class Cart {
-  static addProduct(id, productPrice) {
-    fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
-      if (!err) {
-        cart = JSON.parse(fileContent);
-      }
-
-      const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
-      );
-
-      const existingProduct = cart.products[existingProductIndex];
-      let updateProduct;
-      if (existingProduct) {
-        updateProduct = { ...existingProduct };
-        updateProduct.qty = updateProduct.qty + 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updateProduct;
-      } else {
-        updateProduct = { id: id, qty: 1 };
-        cart.products = [...cart.products, updateProduct];
-      }
-      cart.totalPrice = cart.totalPrice + +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log(err);
-      });
-    });
+const Cart = sequelize.define('cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
   }
+});
 
-  static deleteProduct(id, productPrice) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return;
-      }
-      const updateCart = { ...JSON.parse(fileContent) };
-      const product = updateCart.products.find((prod) => prod.id === id);
-      if(!product){
-        return;
-      }
-      const productQty = product.qty;
-      updateCart.products = updateCart.products.filter(
-        (prod) => prod.id !== id
-      );
-      updateCart.totalPrice = updateCart.totalPrice - productPrice * productQty;
-
-      fs.writeFile(p, JSON.stringify(updateCart), (err) => {
-        console.log(err);
-      });
-    });
-  }
-
-  static getCart(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      const cart = JSON.parse(fileContent);
-      if (err) {
-        cb(null);
-      } else {
-        cb(cart);
-      }
-    });
-  }
-};
+module.exports = Cart;
